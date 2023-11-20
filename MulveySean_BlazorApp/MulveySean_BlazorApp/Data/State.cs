724ba75f72f24af2ec1f8b4d49260cd0a0e48108
SeanMulvey <143544386+SeanMulvey@users.noreply.github.com>
2023-11-19 20:49:52 -0500

    Add files via upload

---
+++ MulveySean_BlazorApp/MulveySean_BlazorApp/Data/State.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MulveySean_BlazorApp.Data
+{
+    public class State
+    {
+        public List<Book> bookList { get; set; } = new List<Book>();
+        public List<User> userList { get; set; } = new List<User>();
+        public List<Book> borrowedList { get; set; } = new List<Book>();
+    }
+}
+
